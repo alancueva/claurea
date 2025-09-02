836121b4432cf6846228bd454e793288daf16d1a
alancuevat <alancuevat@gmail.com>
2025-09-01 20:27:16 -0500

    centrar el logo para mobile y corregir el dropdown de los items

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,10 +15,10 @@ export default function Navbar() {
 
   return (
     <nav className="relative bg-white-200/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          {/* Logo */}
-          <div className="flex justify-items-start sm:hidden">
+      <div className="relative mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-center sm:justify-between">
+          {/* Logo for mobile*/}
+          <div className="flex flex-1 justify-center sm:hidden">
             <a className="rounded-md px-3 py-2 flex items-center" href="#">
               <Image
                 src="/claurea-1-11.png"
@@ -31,11 +31,11 @@ export default function Navbar() {
           </div>
 
           {/* Mobile menu button */}
-          <div className="flex justify-items-end sm:hidden">
+          <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
             <button
               type="button"
               onClick={() => setMenuOpen(!menuOpen)}
-              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500"
+              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-gray-500"
               aria-expanded={menuOpen}
               aria-label="Open main menu"
             >
@@ -108,8 +108,8 @@ export default function Navbar() {
 
         {/* Mobile menu dropdown */}
         {menuOpen && (
-          <div className="sm:hidden mt-2">
-            <div className="flex flex-col items-start space-y-2 bg-slate-100 rounded-md shadow-md p-4">
+          <div className="absolute left-2 right-2 top-16 z-20 sm:hidden">
+            <div className="flex flex-col items-start space-y-2 rounded-md bg-white p-4 shadow-lg ring-1 ring-black ring-opacity-5">
               <Link
                 href="/pages/products"
                 className="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-white/5 hover:text-green-500 flex items-center"
