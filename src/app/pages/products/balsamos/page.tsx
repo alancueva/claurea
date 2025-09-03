0655552880db84880a8b597e5e788e0f277afecd
alancuevat <alancuevat@gmail.com>
2025-09-03 15:40:01 -0500

    secciones no hechas pero estan para mejorar

---
+++ src/app/pages/products/balsamos/page.tsx
@@ -0,0 +1,11 @@
+export default function Balsamos() {
+    return(
+        <div>
+            <h1>Bálsamos</h1>
+            <p>Descubre nuestra línea de bálsamos labiales, formulados con ingredientes naturales para hidratar y proteger tus labios.</p>
+        </div>
+
+    );
+
+
+}
---
+++ src/app/pages/products/serums/page.tsx
@@ -0,0 +1,11 @@
+export default function Serums() {
+    return(
+        <div>
+            <h1>Serums</h1>
+            <p>Descubre nuestra línea de serums, formulados con ingredientes naturales para hidratar y revitalizar tu piel.</p>
+        </div>
+
+    );
+
+
+}
