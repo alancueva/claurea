df7b30170e09ab39f92d538892eb0028734af80e
alancuevat <alancuevat@gmail.com>
2025-09-11 19:01:15 -0500

    seccion para reutilizar y poner un fondo de degradado

---
+++ src/components/sections/section-color-degrade.tsx
@@ -0,0 +1,24 @@
+
+/**
+ * Componente que renderiza una sección con un degradado de colores
+ * @returns {JSX.Element} Un elemento JSX que representa la sección con un degradado de colores
+ */
+
+export default function SectionColorDegrade() {
+  return (
+    <section
+      className={`
+          fixed 
+          inset-0 
+          z-[-1] 
+          bg-gradient-to-b 
+          from-gray-400 
+          to-white
+        `}
+      style={{
+        background: 'linear-gradient(to bottom,rgb(202, 200, 196) 0%,rgb(240, 239, 236) 50%, #ffffff 100%)'
+      }}
+    >
+    </section>
+  );
+}
